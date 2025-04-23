471d00bce8aa38926ff82682ed79e40e0cdcbdc4
xiyuyi <xiyu.yi@gmail.com>
2025-04-23 15:25:39 -0700

    #5 added Qwen2.5-Coder-32B-Instruct minichain, hook-up to ChunkSummary

--- inheritscan/agents/microchains/chains/sumarize_chunk.py
+++ inheritscan/agents/microchains/chains/sumarize_chunk.py
@@ -5,22 +5,30 @@ from langchain.schema.runnable import Runnable
 from typing import Any, Dict, Optional
 
 from inheritscan.agents.microchains.chains.mock import MockChain
-from inheritscan.agents.microchains.shared.llm_config import get_tinyllm150
+from inheritscan.agents.microchains.shared.llm_config import get_qwen_coder_instruct500, get_tinyllm150
 from inheritscan.agents.microchains.prompts.chunk_summary import (
     chunk_summary_prompt_korean,
+    chunk_summary_prompt_english
 )
 
 
 def get_chunk_summary_chain(chain_name):
-    if chain_name == "tinyllama150_korean":
-        llm = get_tinyllm150()
-        # Wrap in a single output parser to maintain JSON formatting
-        chunk_summary_chain = chunk_summary_prompt_korean | llm | StrOutputParser()
-
-        # Wrap in RunnableMap so it's consistent with other chains
-        return RunnableMap({"chunk_summary": chunk_summary_chain})
-
-    elif chain_name == "mock_chain":
+    """
+    implemented the following typs of chain_names:
+    """
+    if chain_name == "mock_chain":
         # Return the mock chain wrapped in RunnableMap
         mock_chain = MockChain()
         return RunnableMap({"chunk_summary": mock_chain})
+    
+    else:
+        if chain_name == "tinyllama150_korean":
+            llm = get_tinyllm150()
+            prompt = chunk_summary_prompt_korean
+
+        elif chain_name == "qwen_coder_32b_instruct500_engilsh":
+            llm = get_qwen_coder_instruct500()
+            prompt = chunk_summary_prompt_english
+
+        chain = prompt | llm | StrOutputParser()
+        return RunnableMap({"chunk_summary": chain})
---
+++ inheritscan/agents/microchains/shared/together_ai.py
@@ -0,0 +1,64 @@
+from typing import Optional, List, Dict, Any
+from langchain_core.language_models.llms import LLM
+from together import Together
+from dotenv import load_dotenv
+
+load_dotenv()  # This will load variables from the .env file into os.environ
+
+
+class TogetherQwenCoder(LLM):
+    model: str = "Qwen2.5-Coder-32B-Instruct"
+    max_tokens: int = (2048,)
+    temperature: int = (0,)
+    top_p: float = (0.95,)
+    min_p: float = (0.05,)
+    top_k: int = (40,)
+    repetition_penalty: float = (1.1,)
+    presence_penalty: int = (0,)
+    frequency_penalty: int = (0,)
+    seed: int = 42
+
+    def __init__(self, **data: Any):
+        super().__init__(**data)
+        # anything thing that takes a string input, spits out str output put.
+        self._client = Together()
+
+    def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
+        # tasek in prompt, return str output
+        # the method under *._llm should vary depends on the llm definition in __init__
+        response = self._client.chat.completions.create(
+            model="Qwen/Qwen2.5-Coder-32B-Instruct",
+            messages=[
+                {"role": "user", "content": prompt},
+            ],
+            max_tokens=self.max_tokens,
+            temperature=self.temperature,
+            top_p=self.top_p,
+            min_p=self.min_p,
+            top_k=self.top_k,
+            repetition_penalty=self.repetition_penalty,
+            presence_penalty=self.presence_penalty,
+            frequency_penalty=self.frequency_penalty,
+            seed=42,
+        )
+        out_str = response.choices[0].message.content
+        return out_str
+
+    @property
+    def _llm_type(self) -> str:
+        return "template"
+
+    @property
+    def _identifying_params(self) -> Dict[str, Any]:
+        return {
+            "model": self.model,
+            "max_tokens": self.max_tokens,
+            "temperature": self.temperature,
+            "top_p": self.top_p,
+            "min_p": self.min_p,
+            "top_k": self.top_k,
+            "repetition_penalty": self.repetition_penalty,
+            "presence_penalty": self.presence_penalty,
+            "frequency_penalty": self.frequency_penalty,
+            "seed": self.seed,
+        }

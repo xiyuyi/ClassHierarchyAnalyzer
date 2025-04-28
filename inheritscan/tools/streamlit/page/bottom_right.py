a68525284adabd9d4d66b32ec873c19a239bacbd
xiyuyi <xiyu.yi@gmail.com>
2025-04-28 12:02:27 -0700

    #7 added button to goto vscode

--- inheritscan/tools/streamlit/page/bottom_right.py
+++ inheritscan/tools/streamlit/page/bottom_right.py
@@ -6,6 +6,7 @@ from inheritscan.tools.uml_panel.entry import render_class_uml
 
 
 def render_bottom_right(context):
+    button1 = streamlit.button("Go to VSCODE", use_container_width=True)
     streamlit.markdown("### 📝 Detailed class description")
     def get_detailed_class_description():
         return """
@@ -32,32 +33,6 @@ def render_bottom_right(context):
     # Render the markdown string
     streamlit.markdown(markdown_text)
 
-    # header = streamlit.markdown("### 🔍 Class details")
-    # button1 = streamlit.button("🔁 Generate AI summaries", use_container_width=True)
-
-    # # Button triggers backend process
-    # if button1:
-    #     streamlit.session_state["generate_ai_summaries"] = (
-    #         True  # Reset the trigger flag
-    #     )
-    #     streamlit.session_state["ai_summary_progress"] = 0  # Reset progress
-
-    #     # If generation is triggered, run process and show progress
-    #     if streamlit.session_state.get("generate_ai_summaries", False):
-    #         progress_bar = streamlit.progress(0, text="Generating AI summaries...")
-    #         summaries_generation_and_render(progress_bar, context)
-    #         streamlit.session_state["generate_ai_summaries"] = (
-    #             False  # Reset the trigger flag
-    #         )
-
-    # # Render the panel normally
-    # uml_diagram_container = streamlit.container()
-    # with uml_diagram_container:
-
-    #     if streamlit.session_state.get("render_uml_diagram", True):
-    #         streamlit.markdown("### 🗺️ Class Hierarchy Diagram")
-    #         result = render_class_uml(context)
-            
-    #         if result:
-    #             streamlit.session_state.update(result)
-    pass
+    if button1:
+        # TODO open the module for this class in vscode.
+        pass

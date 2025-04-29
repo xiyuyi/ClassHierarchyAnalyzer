ee69bdee78aa908cdf449541a357d9cf7112d7df
xiyuyi <xiyu.yi@gmail.com>
2025-04-28 17:20:48 -0700

    # 27 slim down

--- inheritscan/tools/mermaid_panel/entry.py
+++ inheritscan/tools/mermaid_panel/entry.py
@@ -14,13 +14,11 @@ def render_mermaid_panel(context=None):
     # This is the integration entrypoint for the UML rendering panel in the
     # main Class hierarchy explorer Streamlit app.
 
-    # TODO #7: renders the entire mermaid panel.
     button1 = streamlit.container() # generate mermaid graph
     header = streamlit.container() # header, detailed mermaid graph
     mermaid_graph = streamlit.container() # display mermaid graph
-    button2 = streamlit.container() # generate mermaid scripts
     script_box = streamlit.container()
-    button3 = streamlit.container() # copy mermaid scripts to clip-board.
+    button2 = streamlit.container() # copy mermaid scripts to clip-board.
 
     with button1:
         nx_graph = context["detailed_nx_graph"]
@@ -32,14 +30,10 @@ def render_mermaid_panel(context=None):
         streamlit.markdown("### Mermaid Graph:")
 
     with mermaid_graph:
-        # mermaid_scripts = load_mermaid_scripts()
         render_mermaid_graph(mermaid_script)
-
-    with button2:
-        streamlit.button("🔁 Generate Mermaid Script", use_container_width=True)
 
     with script_box:
         streamlit.markdown(" - Here comes the mermaid scripts")
 
-    with button3:
-         streamlit.button("📋 Copy Mermaid Script", use_container_width=True)
+    with button2:
+         streamlit.button("📋 Copy Mermaid Script to Clipboard", use_container_width=True)

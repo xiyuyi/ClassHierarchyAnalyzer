588185f3a992d32bbcf8cd7a1a2efe8224d80652
xiyuyi <xiyu.yi@gmail.com>
2025-04-30 17:00:30 -0700

    #24 clean up logic for bottom left

--- inheritscan/tools/streamlit/page/bottom_left.py
+++ inheritscan/tools/streamlit/page/bottom_left.py
@@ -8,9 +8,33 @@ from inheritscan.tools.uml_panel.entry import render_class_uml
 def render_bottom_left(context):
     # define layout:
     header = streamlit.container()
-    button1 = streamlit.container()  # generate ai summaries
-    # Button triggers backend process
-    if button1:
+    button1 = streamlit.container()
+    progress = streamlit.container()
+    graph_display = streamlit.container()
+
+    # render each component:
+    with header:
+        streamlit.markdown("### 🔍 Detailed Class View")
+
+    with button1:
+        b1_handle = streamlit.button(
+            "🔁 Generate AI summaries", use_container_width=True
+        )
+
+    with progress:
+        progress_bar = streamlit.progress(0, text="Generating AI summaries...")
+
+    with graph_display:
+        streamlit.markdown("### 🗺️ Class Hierarchy Diagram")
+        result = render_class_uml(context)
+        print("finished rendering class uml, result:")
+        print(result)
+        if result:
+            streamlit.session_state.update(result)
+
+    # define actions
+    if b1_handle:
+        print("pressed the 🔁 Generate AI summaries button!")
         # streamlit.session_state["generate_ai_summaries"] = (
         #     True  # Reset the trigger flag
         # )
@@ -18,28 +42,7 @@ def render_bottom_left(context):
 
         # If generation is triggered, run process and show progress
         if streamlit.session_state.get("generate_ai_summaries", False):
-            progress_bar = streamlit.progress(
-                0, text="Generating AI summaries..."
-            )
             summaries_generation_and_render(progress_bar, context)
             # streamlit.session_state["generate_ai_summaries"] = (
             #     False  # Reset the trigger flag
             # )
-    graph_display = streamlit.container()
-    # render each element:
-    with header:
-        streamlit.markdown("### 🔍 Detailed Class View")
-
-    with button1:
-        streamlit.button("🔁 Generate AI summaries", use_container_width=True)
-
-    # Render the panel normally
-    with graph_display:
-        uml_diagram_container = streamlit.container()
-        with uml_diagram_container:
-            streamlit.markdown("### 🗺️ Class Hierarchy Diagram")
-            result = render_class_uml(context)
-            print("finished rendering class uml, result:")
-            print(result)
-            if result:
-                streamlit.session_state.update(result)
